24508eed897f3cbeffe2ab4225ba38560c44737f
duy.vo <vobaoduy123@gmail.com>
2023-11-05 15:47:20 +0000

    feat(formatter linter document): setting up formmater linter and write documment

--- src/routes/paths.route.jsx
+++ src/routes/paths.route.jsx
@@ -1,6 +1,8 @@
+import React from 'react';
 import { Home } from '../pages/Home';
 import { AboutUs } from '../pages/AboutUs';
 import { routeNames } from '../config';
+
 export const routePaths = {
   public: [
     {
