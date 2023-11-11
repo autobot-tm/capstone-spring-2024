f84fab04472c7c8f170eec7200975733feb2da30
duy.vo <vobaoduy123@gmail.com>
2023-11-11 11:40:29 +0700

    fix(layout-header-top): fix ui

--- src/hoc/Layout/components/LayoutTopSection/LayoutTopSection.jsx
+++ src/hoc/Layout/components/LayoutTopSection/LayoutTopSection.jsx
@@ -1,30 +1,27 @@
 import React from 'react';
-import { Layout, Row, Col, Button } from 'antd';
+import { Layout, Row, Col } from 'antd';
 const { Content } = Layout;
 import './styles.scss';
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone } from '@fortawesome/free-solid-svg-icons';
 import { DoubleRightOutlined } from '@ant-design/icons';
-import { useTranslation } from 'react-i18next';
 
 export const LayoutTopSection = () => {
   const { t } = useTranslation();
   return (
     <Layout id="layout-top-section">
       <Content className="container">
-        <Row>
-          <Col className="ant-col" xs={24} sm={24} md={5} lg={5} xl={5}>
-            <div className="navbar-brand"></div>
+        <Row gutter={12}>
+          <Col className="logo" xs={24} sm={24} md={6}>
+            <Link to="/">
+              <div className="navbar-brand"></div>
+            </Link>
           </Col>
 
-          <Col className="ant-col ant-col-xs" xs={24} sm={24} md={6} lg={6} xl={6}>
-            <a className="phone-header-section" href="tel:+842838275068">
-              +84(0)28 3827 5068
-            </a>
-          </Col>
-
-          <Col className="ant-col ant-col-xl" xs={0} sm={0} md={4} lg={4} xl={4}>
-            <p style={{ fontSize: 12, whiteSpace: 'nowrap' }}>
+          <Col className="info hide-on-sm" xs={0} sm={0} md={6}>
+            <p className="info-text">
               {t`top-section-establishment.line-1`}
               <br />
               {t`top-section-establishment.line-2`}
@@ -35,31 +32,41 @@ export const LayoutTopSection = () => {
             </p>
           </Col>
 
-          <Col className="ant-col ant-col-xl ant-col-lg" xs={24} sm={24} md={7} lg={7} xl={7}>
-            <div className="contact-layout">
-              <i>
-                <FontAwesomeIcon className="phone-icon" icon={faPhone} />
-              </i>
-              <span>
-                {t`top-section-contact-1`}:<a href="tel:028 3827 5068"> 028 3827 5068</a> <br />
-                {t`top-section-contact-2`}:<a href="tel: +84(0)28 3827 5068"> +84(0)28 3827 5068</a>
-              </span>
+          <Col className="contact hide-on-sm" sm={0} md={5}>
+            <FontAwesomeIcon className="contact-phone-icon" icon={faPhone} />
+            <div className="contact-info">
+              <p className="contact-info-group">
+                <span>
+                  {t`top-section-contact-1`}:{' '}
+                  <a href="tel:028 3827 5068" className="contact-link">
+                    {' '}
+                    028 3827 5068
+                  </a>
+                </span>
+              </p>
+              <p className="contact-info-group">
+                <span>
+                  {t`top-section-contact-2`}:
+                  <a href="tel: +84(0)28 3827 5068" className="contact-link">
+                    {' '}
+                    +84(0)28 3827 5068
+                  </a>
+                </span>
+              </p>
             </div>
           </Col>
 
-          <Col className="ant-col ant-col-xl ant-col-lg" md={6} lg={6} xl={6} span={6}>
-            <Button
-              size="small"
-              className="btn-top-section"
-              type="primary"
-              onBlur={() => {
-                alert('Đang chuyển đến Contact page');
-              }}>
-              <span className="btn-component-section">
-                <p>{t`top-section-support`}</p>
-                <DoubleRightOutlined style={{ fontSize: '30px' }} />
-              </span>
-            </Button>
+          <Col className="contact-button-container hide-on-sm" sm={0} md={6}>
+            <Link to="/" className="contact-button">
+              <p className="contact-button-text">{t`top-section-support`}</p>
+              <DoubleRightOutlined className="contact-button-icon" />
+            </Link>
+          </Col>
+          <Col className="phone-container hide-upper-sm" xs={24} sm={24} md={0}>
+            <a className="phone-header" href="tel:+842838275068">
+              <FontAwesomeIcon className="phone-icon" icon={faPhone} />
+              <span>+84(0)28 3827 5068</span>
+            </a>
           </Col>
         </Row>
       </Content>
