aaf5f897f2a0a940a7974292adbe6f52149d4795
duy.vo <vobaoduy123@gmail.com>
2024-01-27 08:12:29 +0000

    Cap 62: SETUP Typography components

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,7 @@ import React, { createContext } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Layout } from '../../hoc/Layout';
 import { TabName } from '../../components/TabName';
-
+import { Caption, Headline, Paragraph, SubHeading } from '../../components/Typography';
 import './styles.scss';
 
 export const ModalContext = createContext();
@@ -16,6 +16,25 @@ export const Home = () => {
       <div className="home-container">
         <h1>{t('home')}</h1>
         <h2>{t('welcome-text')}</h2>
+      </div>
+      <div>
+        <Headline size={600}>Headline 600</Headline>
+        <Headline size={450}>Headline 450</Headline>
+        <SubHeading size={260} strong>
+          SubHeading 260 bold
+        </SubHeading>
+        <SubHeading size={260}>SubHeading 260</SubHeading>
+        <SubHeading size={230} strong>
+          SubHeading 230
+        </SubHeading>
+        <SubHeading size={230}>SubHeading 230</SubHeading>
+        <Paragraph strong>Paragraph 160 Bold</Paragraph>
+        <Paragraph>Paragraph 160</Paragraph>
+        <Caption size={140} strong>
+          Caption 140 Bold
+        </Caption>
+        <Caption size={140}>Caption 140</Caption>
+        <Caption size={110}>Caption 110</Caption>
       </div>
     </Layout>
   );
