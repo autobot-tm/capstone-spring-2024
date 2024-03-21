647f2420659150941342af7fc3b8b8c1ee61bdce
Hoàng Long <longnguyen5112000@gmail.com>
2024-03-21 04:03:15 +0000

    CAP-252

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import { PrivateRoute, routePaths } from './routes';
 import { useDispatch, useSelector } from 'react-redux';
-import { useAuthSlice } from './store/slices';
+import { signOut, useAuthSlice } from './store/slices';
 import { isTimeExpired } from './utils/time';
 import { REQUEST_TIME_OUT } from './constants/api.constant';
 import ScrollToTop from './components/ScrollToTop/ScrollToTop'; // Import ScrollToTop
@@ -11,6 +11,7 @@ import { getMetaData } from './services/apis/houses.service';
 import { setMetaData } from './store/slices/houseSlice';
 import NotFoundPage from './pages/NotFound/NotFoundPage';
 import { useUserSlice } from './store/slices/user.slice';
+import { ERROR_TRANS_KEYS } from './constants/error.constant';
 
 function App() {
   const dispatch = useDispatch();
@@ -19,7 +20,8 @@ function App() {
   const [isTokenRefreshed, setIsTokenRefreshed] = useState(false);
   const { access_token, access_token_expires_at } = useSelector(state => state.auth);
   const [loading, setLoading] = useState(true);
-
+  const { errorTranslationKey } = useSelector(state => state.auth);
+  const navigate = useNavigate();
   useEffect(() => {
     if (!access_token) {
       dispatch(authActions.initState());
@@ -31,11 +33,19 @@ function App() {
 
   useEffect(() => {
     const isAccessTokenExpired = isTimeExpired(access_token_expires_at, REQUEST_TIME_OUT);
+
     if (access_token && !isTokenRefreshed && isAccessTokenExpired) {
       setIsTokenRefreshed(true);
       dispatch(authActions.refreshToken());
     }
   }, [access_token]);
+
+  useEffect(() => {
+    if (errorTranslationKey === ERROR_TRANS_KEYS.ACCOUNT_SUSPENDED) {
+      navigate('/');
+      dispatch(signOut());
+    }
+  }, [errorTranslationKey]);
 
   useEffect(() => {
     getMetaData()
