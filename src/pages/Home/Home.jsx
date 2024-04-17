35c13c950a555300f68d0f96255457fd02a76dd5
Hoàng Long <longnguyen5112000@gmail.com>
2024-04-17 11:57:51 +0000

    CAP-369

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,22 +1,30 @@
 import './styles.scss';
 import React from 'react';
 import { Layout } from '../../hoc/Layout';
-import { imagesHomePage } from '../../assets/images';
 import HomeCarousel from './components/HomeCarousel/HomeCarousel';
 import Houses from './components/Houses/Houses';
 import { Helmet } from 'react-helmet';
 import { useTranslation } from 'react-i18next';
+import useSWR from 'swr';
+import { getHousesService } from '../../services/apis/houses.service';
 
 export const Home = () => {
+  const LIMIT = 6;
   const { t } = useTranslation();
+  const { data, isLoading } = useSWR(`getHousesService`, async () => {
+    return await getHousesService({
+      offset: 0,
+      limit: LIMIT,
+    });
+  });
   return (
     <Layout>
       <Helmet>
         <title>{t('title-tab-main')}</title>
       </Helmet>
-      <HomeCarousel images={imagesHomePage} />
+      <HomeCarousel data={data} />
       <div className="home-container">
-        <Houses />
+        <Houses data={data} isLoading={isLoading} limit={LIMIT} />
       </div>
     </Layout>
   );
